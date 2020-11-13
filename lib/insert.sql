84aff34eee7ebbfe24d2bbe4affdce37ee8d8fe8
John Ratana <jratana1@gmail.com>
2020-11-13 18:58:07 -0500

    commit tree f4dcc9dc457aa12d658c7d1e4d6cfb0700f13351

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Lil' Bub", 5, 'M', "BROWN", "COOL", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("ARDEN", 3, "F", "BLACK", "WHINY", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("WALDEN", 5, "M", "BROWN", "COOL", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("SUMMER", 28, "F", "BROWN", "COOL", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("VIVIAN", 36, "F", "BLACK", "TENSE", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, "M", "BROWN", "AGGRESSIVE", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("JOHN", 36, "M", "BLACK", "COOL", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("LISA", 32, "F", "BROWN", "COOL", 1);
